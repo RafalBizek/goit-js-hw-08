d8e174157db2e30e31a0f16b0a1a5cd5554b3cc8
Rafał Biżek <rafal.bizek@gmail.com>
2023-04-23 11:34:24 +0200

    update/task02 and 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// funkcja zapisuje czas odtwarzacza w local storage
+const saveTimeLocalStorage = time => {
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+// funkcja do odtwarzania czasu odtwarzania z local storage
+const getTimeFromLocalStorage = () => {
+  return localStorage.getItem('videoplayer-current-time');
+};
+
+// rejestracja zdarzenia timeupdate raz na sekundę
+player.on(
+  'timeupdate',
+  throttle(() => {
+    // pobieram aktualny czas odtwarzania
+    player.getCurrentTime().then(time => {
+      // zapisuję czas odtwarzania w local storage
+      saveTimeLocalStorage(time);
+    });
+  }, 1000)
+);
+
+// po załadowaniu strony sprawdzam czy jest zapisany czas odtwarzania
+// w local storage
+const storedTime = getTimeFromLocalStorage();
+
+// jeśli jest zapisany ustaw jako domyślny czas odtwarzania
+if (storedTime !== null) {
+  player.setCurrentTime(storedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+const form = document.querySelector('.feedback-form');
+const emailField = form.querySelector('input[name="email"]');
+const messageField = form.querySelector('textarea[name="message"]');
 
+form.addEventListener('input', () => {
+  const formData = {
+    email: emailField.value,
+    message: messageField.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+window.addEventListener('load', () => {
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formData) {
+    emailField.value = formData.email;
+    messageField.value = formData.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = {
+    email: emailField.value,
+    message: messageField.value,
+  };
+  console.log('Form submitted with data:', formData);
+  localStorage.removeItem('feedback-form-state');
+  emailField.value = '';
+  messageField.value = '';
+});
+
+import throttle from 'lodash.throttle';
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const formData = {
+      email: emailField.value,
+      message: messageField.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }, 500)
+);
